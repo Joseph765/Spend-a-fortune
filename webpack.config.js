587da1ec8dac23e80777294e16e83df02f190168
Joseph765 <joseph.chauvin102@gmail.com>
2021-08-09 18:22:51 -0500

    changed to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, 'src/Index.jsx'),
   output: {
     path: path.resolve(__dirname, 'public'),
