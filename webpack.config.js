3f9d705ade83b8f794953a332d60f027f6b77878
Joseph765 <joseph.chauvin102@gmail.com>
2021-08-09 18:10:35 -0500

    changed webpack with a capital I

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.jsx'),
+  entry: path.resolve(__dirname, 'src/Index.jsx'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
