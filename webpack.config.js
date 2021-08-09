8dc6ce5e53346759fdb2f96954ab59370259ac06
Joseph765 <joseph.chauvin102@gmail.com>
2021-08-09 17:40:24 -0500

    editing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.m?jsx$/,
-        exclude: /(node_modules)/,
+        exclude: /(node_modules|browser_components)/,
         use: {
           loader: 'babel-loader',
           options: {
